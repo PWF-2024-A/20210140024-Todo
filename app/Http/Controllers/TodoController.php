965f7490c3394069d22681973d2f1b957c3deb25
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2024-04-15 21:23:51 +0700

    update, delete data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -15,12 +15,14 @@ class TodoController extends Controller
         $todos = Todo::where('user_id', auth()->user()->id)
             ->orderBy('is_complete', 'asc')
             ->orderBy('created_at', 'desc')
-            ->paginate(3);
-
+            ->get();
 
         // dd($todos);
 
-        return view('todo.index', compact('todos'));
+        $todosCompleted = Todo::where('user_id', auth()->user()->id)
+            ->where('is_complete', true)
+            ->count();
+        return view('todo.index', compact('todos', 'todosCompleted'));
     }
 
     /**
@@ -44,7 +46,6 @@ class TodoController extends Controller
             'title' => ucfirst($request->title),
             'user_id' => auth()->user()->id,
         ]);
-        $todo->save();
 
         return redirect()->route('todo.index')->with('success', 'Todo created successfully!');
     }
@@ -54,7 +55,7 @@ class TodoController extends Controller
      */
     public function show(Todo $todo)
     {
-        //
+        //nampilin satu2
     }
 
     /**
@@ -62,7 +63,14 @@ class TodoController extends Controller
      */
     public function edit(Todo $todo)
     {
-        return view('todo.edit');
+        if (auth()->user()->id == $todo->user_id) {
+            // dd($todo);
+            return view('todo.edit', compact('todo'));
+        } else {
+            // abort(403);
+            // abort(403, 'Not authorized');
+            return redirect()->route('todo.index')->with('danger','You are not authorized to edit this todo!');
+        }
     }
 
     /**
@@ -70,7 +78,44 @@ class TodoController extends Controller
      */
     public function update(Request $request, Todo $todo)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        // Practical
+        // $todo->title = $request->title;
+        // $todo->save();
+
+        // Eloquent Way - Readable
+        $todo->update([
+            'title' => ucfirst($request->title),
+        ]);
+        return redirect()->route('todo.index')->with('success', 'Todo updated successfully!');
+    }
+
+    public function complete(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->update([
+                'is_complete' => true,
+            ]);
+            return redirect()->route('todo.index')->with('success', 'Todo completed successfully!');
+        } else {
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to complete this todo!');
+        }
+    }
+
+    public function uncomplete(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+
+            $todo->update([
+                'is_complete' => false,
+            ]);
+            return redirect()->route('todo.index')->with('success', 'Todo uncompleted successfully!');
+        } else {
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to uncomplete this todo!');
+        }
     }
 
     /**
@@ -78,6 +123,24 @@ class TodoController extends Controller
      */
     public function destroy(Todo $todo)
     {
-        //
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->delete();
+            return redirect()->route('todo.index')->with('success', 'Todo deleted successfully!');
+        } else {
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to delete this todo!');
+        }
+    }
+
+    public function destroyCompleted()
+    {
+        // get all todos for current user where is_completed is true
+        $todosCompleted = Todo::where('user_id', auth()->user()->id)
+            ->where('is_complete', true)
+            ->get();
+        foreach ($todosCompleted as $todo) {
+            $todo->delete();
+        }
+        // dd($todosCompleted);
+        return redirect()->route('todo.index')->with('success', 'All completed todos deleted successfully!');
     }
 }
